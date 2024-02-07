6a3ae98c1cd9f6b1b0cce4ced89058cd28c00657
Somya Sarathi Samal <soms9samal@gmail.com>
2024-02-07 21:05:13 +0530

    Building documentation scraper - filtering the desired tags from documents. - Need to convert html to markdown and replace header tags with   delimeters to separate chuncks.

--- code/scrape.py
+++ code/scrape.py
@@ -1,24 +1,37 @@
 import requests
 from typing import List
+from functools import reduce
 from bs4 import BeautifulSoup as bs
 
+class DocScrapper:
+    def __init__(self) -> None:
+        self.URL = 'https://learn.microsoft.com/en-us/power-bi/guidance/'
+        self.HEADERS = {"User-Agent": "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254"}
+        self.INCLUDE = {'Power BI guidance', 'Transform and shape data', 'Data modeling', 'DAX', 'Power BI reports'}
 
-def topic_url_list() -> List[str]:
-    '''
-    Returns a list of urls to scrape
-    '''
-    URL = 'https://learn.microsoft.com/en-us/power-bi/guidance/'
-    HEADERS = {"User-Agent": "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254"}
+
+    def topic_url_list(self) -> List[str]:
+        '''
+        Returns a list of urls to scrape
+        '''
+        # Extracting urls to crawl
+        links = [ a['href'] for a in bs(requests.get(url=self.URL, headers=self.HEADERS).text, 'lxml').findAll(class_='has-external-link-indicator') if a.parent.parent.parent.h2.text in self.INCLUDE ]
+
+        return links
+
+
+    def url_to_doc(self, url: str) -> None:
+        # TODO: convert html to markdown
+        response = requests.get(self.URL+url, headers=self.HEADERS)
+        soup = bs(response.text, 'lxml')
+        
+        page = reduce(lambda x,y: x+y, map(lambda x: str(x), filter(lambda x: x.name!='div' and x.name!='h2' and x.name!='h3', soup.find('div', class_='content').contents)))
+
+        return page
     
-    # List of topics to include
-    INCLUDE = {'Power BI guidance', 'Transform and shape data', 'Data modeling', 'DAX', 'Power BI reports'}
+    def gen_doc(self):
+        # TODO: generate documentation topic_url_list+url_to_doc
+        pass
 
 
-    page = requests.get(url=URL, headers=HEADERS)
-
-    soup = bs(page.text, 'lxml')
-    links = soup.findAll(class_='has-external-link-indicator')
-
-    links = [URL+a['href'] for a in links if a.parent.parent.parent.h2.text in INCLUDE ]
-    
-    return links
+print(DocScrapper().url_to_doc('https://learn.microsoft.com/en-us/power-bi/guidance/report-drillthrough'))
